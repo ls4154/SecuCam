bc298351310d73e50769b3b7de4e9703f83ddced
Youngjae Lee <steel4154@gmail.com>
2019-06-22 16:27:16 +0900

    Servo with sysfs

--- driver/servo/servo.c
+++ driver/servo/servo.c
@@ -3,6 +3,8 @@
 #include <linux/gpio.h>
 #include <linux/hrtimer.h>
 #include <linux/sched.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
 
 #define PWM_PERIOD 20000000 /* 20 ms */
 #define MIN_DUTY_CYCLE 1000000
@@ -10,12 +12,41 @@
 
 #define GPIO_SERVO 22
 
-struct class *servo_class;
+static void set_angle(unsigned int angle);
 
 static struct hrtimer htimer;
 static ktime_t kt0, kt1;
 
 static int state = 0;
+static unsigned int value_angle;
+
+static ssize_t servo_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", value_angle);
+}
+
+static ssize_t servo_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count)
+{
+	int val;
+	sscanf(buf, "%u", &val);
+	set_angle(val);
+	return count;
+}
+
+
+static struct kobject *servo_kobj;
+
+static struct kobj_attribute servo_attr = 
+__ATTR(servo0, 0660, servo_show, servo_store);
+
+static struct attribute *servo_attrs[] = {
+	&servo_attr.attr,
+	NULL
+};
+
+static struct attribute_group servo_attr_group = {
+	.attrs = servo_attrs
+};
 
 static enum hrtimer_restart function_timer(struct hrtimer *unused)
 {
@@ -38,15 +69,18 @@ static void set_cycle(unsigned int cycle)
 		cycle = MIN_DUTY_CYCLE;
 
 	kt0 = ktime_set(0, PWM_PERIOD - cycle);
-	kt1 = ktime_set(0, PWM_PERIOD);
+	kt1 = ktime_set(0, cycle);
 }
 
 static void set_angle(unsigned int angle)
 {
 	int cycle;
 
+
 	if (angle > 180)
 		angle = 180;
+
+	value_angle = angle;
 
 	cycle = angle * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE) / 180 + MIN_DUTY_CYCLE;
 	
@@ -59,17 +93,21 @@ static int __init servo_init(void)
 
 	state = 0;
 
-	/* register class */
-	servo_class = class_create(THIS_MODULE, "servo_class");
-	if (IS_ERR(servo_class))
+	/* register */
+	servo_kobj = kobject_create_and_add("servo_kobj", kernel_kobj);
+	if (!servo_kobj)
 		goto err;
+	if (sysfs_create_group(servo_kobj, &servo_attr_group)) {
+		kobject_put(servo_kobj);
+		goto err;
+	}
 
 	/* init gpio */
 	gpio_request(GPIO_SERVO, "servo");
 	gpio_direction_output(GPIO_SERVO, 0);
 
 	/* set ktime */
-	set_cycle(MIN_DUTY_CYCLE);
+	set_angle(90);
 
 	/* init hrtimer */
 	hrtimer_init(&htimer, CLOCK_REALTIME, HRTIMER_MODE_REL);
@@ -77,8 +115,9 @@ static int __init servo_init(void)
 	hrtimer_start(&htimer, kt0, HRTIMER_MODE_REL);
 
 	return 0;
+
 err:
-	class_unregister(servo_class);
+	printk("init error");
 	return -1;
 }
 
@@ -89,7 +128,7 @@ static void __exit servo_exit(void)
 
 	hrtimer_cancel(&htimer);
 
-	class_unregister(servo_class);
+	kobject_put(servo_kobj);
 
 	printk(KERN_INFO "servo : exit\n");
 }
@@ -101,4 +140,3 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Servo Driver");
 MODULE_AUTHOR("Youngjae Lee");
 
-// https://lwn.net/Articles/167897/
